be38962f365048b4937a2b0b26909a5c665142d6
Evgeniy Tsekhmistro <evgeniy.tsekhmistro@gmail.com>
2019-06-21 12:10:05 +0300

    Added working sidebar

---
+++ src/components/AddCategory.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function AddRootCategory() {
+    return (
+        <form action="GET">
+            <input />
+            <button>Add category</button>
+        </form>
+    )
+}
+
+export default AddRootCategory;
